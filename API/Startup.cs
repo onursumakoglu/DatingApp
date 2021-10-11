b1e0e575c986fb173ebca425767f73d6c1da85d2
Onur Sumakoğlu <sumakogluonur@gmail.com>
2021-10-11 19:31:59 +0300

    angular downloaded, made https request in angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,10 +34,12 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+ 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            
+            app.UseCors(xpolicy => xpolicy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
